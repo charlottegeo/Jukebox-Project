96ad8810951dc3272d1b07c6afdd43345a8d2345
charlottegeo <cngg805@gmail.com>
2023-09-24 10:47:34 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'songs'
+    'songs',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
