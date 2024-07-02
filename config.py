4b54ce90b2cf5bca5d56db8e31c0324f151300fa
Charlotte George <cngg805@gmail.com>
2024-07-02 19:05:46 -0400

    changed port from 5000 to 8080

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config:
     OIDC_ISSUER = os.environ.get("OIDC_ISSUER", "https://sso.csh.rit.edu/auth/realms/csh")
     OIDC_CLIENT_ID = os.environ.get("OIDC_CLIENT_ID", "catjam-app")
     OIDC_CLIENT_SECRET = os.environ.get("OIDC_CLIENT_SECRET", "NOT-A-SECRET")
-    OIDC_REDIRECT_URI = os.environ.get("OIDC_REDIRECT_URI", "http://localhost:5000/oidc_callback")
+    OIDC_REDIRECT_URI = os.environ.get("OIDC_REDIRECT_URI", "http://localhost:8080/oidc_callback")
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import socketio
 if __name__ == '__main__':
     from app import create_app
     app = create_app()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=8080)
