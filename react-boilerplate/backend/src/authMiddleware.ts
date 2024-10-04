729b8760db062547cebea421df31515b9531633c
charlottegeo <cngg805@gmail.com>
2024-10-04 10:22:47 -0400

    Moved files from react-boilerplate

--- react-boilerplate/backend/src/authMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const authenticatedOnly = (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session || !req.session.uid) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-    next();
-};
